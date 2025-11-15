863dc10402d21526280130d8d2bc24ca987b4e10
vivanvj21 <125850020+vivanvj21@users.noreply.github.com>
2025-11-15 11:35:36 +0530

    feat(sql): write SQL window functions for running totals and 7d moving average

--- 04-data-analytics/window-functions.sql
+++ 04-data-analytics/window-functions.sql
@@ -12,3 +12,5 @@ Production module delivering verified patterns, unit tests, and performance benc
 ### Activity & Changelog:
 
 - **[2025-10-28 18:35]** write SQL window functions for running totals and 7d moving average (Commit #46)
+
+- **[2025-11-15 11:35]** write SQL window functions for running totals and 7d moving average (Commit #99)
