f11103eb8d5b0ad383a26f39a8148cff80b98469
vivanvj21 <125850020+vivanvj21@users.noreply.github.com>
2025-12-17 17:34:31 +0530

    refactor(ds): modularize feature transformation pipelines using scikit-learn Pipeline

--- 02-data-science/pipeline-modules.py
+++ 02-data-science/pipeline-modules.py
@@ -15,3 +15,4 @@ Production module delivering verified patterns, unit tests, and performance benc
 
 - **[2025-11-11 18:03]** modularize feature transformation pipelines using scikit-learn Pipeline (Commit #82)
 - **[2025-11-29 10:04]** modularize feature transformation pipelines using scikit-learn Pipeline (Commit #135)
+- **[2025-12-17 17:34]** modularize feature transformation pipelines using scikit-learn Pipeline (Commit #188)
