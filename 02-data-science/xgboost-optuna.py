e2fdeb036ed7f20a381d95901f1a10307b7307e7
vivanvj21 <125850020+vivanvj21@users.noreply.github.com>
2026-02-13 21:31:38 +0530

    feat(ds): configure Optuna Bayesian hyperparameter search for XGBoost

--- 02-data-science/xgboost-optuna.py
+++ 02-data-science/xgboost-optuna.py
@@ -24,3 +24,4 @@ Production module delivering verified patterns, unit tests, and performance benc
 - **[2026-01-07 15:22]** tune subsample and colsample_bytree to mitigate overfitting (Commit #234)
 - **[2026-01-26 16:03]** configure Optuna Bayesian hyperparameter search for XGBoost (Commit #286)
 - **[2026-01-26 19:55]** tune subsample and colsample_bytree to mitigate overfitting (Commit #287)
+- **[2026-02-13 21:31]** configure Optuna Bayesian hyperparameter search for XGBoost (Commit #339)
