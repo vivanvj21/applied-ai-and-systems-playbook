e84f2e3d101b132b62f78a4fce5e46834e9944f0
vivanvj21 <125850020+vivanvj21@users.noreply.github.com>
2026-02-16 09:04:15 +0530

    tune(ml): tune subsample and colsample_bytree to mitigate overfitting

--- 02-data-science/xgboost-optuna.py
+++ 02-data-science/xgboost-optuna.py
@@ -25,3 +25,4 @@ Production module delivering verified patterns, unit tests, and performance benc
 - **[2026-01-26 16:03]** configure Optuna Bayesian hyperparameter search for XGBoost (Commit #286)
 - **[2026-01-26 19:55]** tune subsample and colsample_bytree to mitigate overfitting (Commit #287)
 - **[2026-02-13 21:31]** configure Optuna Bayesian hyperparameter search for XGBoost (Commit #339)
+- **[2026-02-16 09:04]** tune subsample and colsample_bytree to mitigate overfitting (Commit #340)
