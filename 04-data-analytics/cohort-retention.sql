79c0398676eb43770352a970bf0c0b1cb6100f6c
vivanvj21 <125850020+vivanvj21@users.noreply.github.com>
2026-02-22 18:38:55 +0530

    feat(sql): create cohort retention analysis queries using recursive CTEs

--- 04-data-analytics/cohort-retention.sql
+++ 04-data-analytics/cohort-retention.sql
@@ -18,3 +18,4 @@ Production module delivering verified patterns, unit tests, and performance benc
 - **[2025-12-30 17:30]** create cohort retention analysis queries using recursive CTEs (Commit #210)
 - **[2026-01-19 19:42]** create cohort retention analysis queries using recursive CTEs (Commit #263)
 - **[2026-02-05 17:48]** create cohort retention analysis queries using recursive CTEs (Commit #316)
+- **[2026-02-22 18:38]** create cohort retention analysis queries using recursive CTEs (Commit #369)
